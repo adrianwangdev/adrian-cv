b67eb0bd2d8369349141e633d5086a97d1d40169
= <=>
2021-02-04 20:06:25 +0800

    fixed address (craco)

--- craco.config.js
+++ craco.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
   webpack: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': path.resolve(__dirname, 'src/'),
       '@images': path.resolve(__dirname, 'src/assets/images/'),
       '@components': path.resolve(__dirname, 'src/components/'),
       '@themes': path.resolve(__dirname, 'src/themes/'),
