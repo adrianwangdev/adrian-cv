bb62f29847e40a6971376ab3092d02e5f96df0ac
Adrian <wry608.work@gmail.com>
2021-03-25 13:57:55 +0800

    feat: build contact page

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -7,10 +7,14 @@ import SectionTitle from '@components/SectionTitle/SectionTitle'
 
 /* Styles */
 import { SectionWrapper } from '@components/StyledModule/SectionWrapper.styled'
+import { Row, Column } from '@components/StyledModule/Grid.styled'
 import { MenuButton } from '@components/StyledModule/MenuButton.styled'
+import { Card, TextContent } from '@components/Contact/Contact.styled'
 
 /* Images */
 import { ReactComponent as MenuIcon } from '@images/nav/menu.svg'
+import { ReactComponent as MailIcon } from '@images/contact/mail.svg'
+import { ReactComponent as PhoneIcon } from '@images/contact/phone.svg'
 
 const Contact = () => {
 
@@ -33,6 +37,25 @@ const Contact = () => {
         Contact
         <span>聯絡方式</span>
       </SectionTitle>
+
+      <Row>
+        <Column>
+          <Card href="mailto:adrianwangdev@gmail.com">
+            <MailIcon />
+          </Card>
+        </Column>
+        <Column>
+          <Card href="tel:0910319855">
+            <PhoneIcon />
+          </Card>
+        </Column>
+        <Column $fullWidth>
+          <TextContent>
+            <h3>Thank you</h3>
+            <p>非常感謝您所花費的時間</p>
+          </TextContent>
+        </Column>
+      </Row>
 
     </SectionWrapper>
   )
